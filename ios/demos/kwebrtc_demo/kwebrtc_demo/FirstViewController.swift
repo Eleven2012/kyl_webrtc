fcf3ac230858d3a2fa43a4cc5b1c40ce49db6991
yulu.kong <yulu.kong@ubtrobot.com>
2019-09-05 09:09:00 +0800

    初始化项目

---
+++ ios/demos/kwebrtc_demo/kwebrtc_demo/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  kwebrtc_demo
+//
+//  Created by yulu kong on 2019/9/5.
+//  Copyright © 2019 yulu kong. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/demos/kwebrtc_demo/kwebrtc_demo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  kwebrtc_demo
+//
+//  Created by yulu kong on 2019/9/5.
+//  Copyright © 2019 yulu kong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
